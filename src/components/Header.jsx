8300117a2b3c3c9bcab108b583d4ad45842b60bf
Igor Trindade <84097839+igortrindadelg@users.noreply.github.com>
2022-03-24 16:02:02 -0300

    navbar started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import '../scss/Header.scss'
 
 const Header = () => {
-    return ( <></> );
+    return (
+        <nav className='navbar'>
+            <div className="logo">
+                <h2>Igor Trindade.dev</h2>
+            </div>
+
+            <ul className="navLinks">
+                <li className="navLink"><a href="#">Home</a></li>
+                <li className="navLink"><a href="#">About</a></li>
+                <li className="navLink"><a href="#">Services</a></li>
+                <li className="navLink"><a href="#">Projects</a></li>
+                <li className="navLink">
+                    <button className="navButton">Contact</button>
+                </li>
+            </ul>
+        </nav>
+    );
 }
  
 export default Header;
