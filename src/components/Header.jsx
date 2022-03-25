6a872e6467bf67dc7a5525f2a3d117b9eec31d45
Igor Trindade <84097839+igortrindadelg@users.noreply.github.com>
2022-03-25 16:23:21 -0300

    responsive navbar added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,44 @@
 import React from 'react'
+import { useState } from 'react'
+
 import '../scss/Header.scss'
 
 const Header = () => {
+    const [active, setActive] = useState('navLinks')
+    const [ toggleIcon, setToggleIcon ] = useState('navToggle')
+
+    const navToggle = () => {
+        active === 'navLinks' 
+        ? setActive('navLinks navActive') 
+        : setActive('navLinks') 
+
+        toggleIcon === 'navToggle'
+        ? setToggleIcon('navToggle toggle')
+        : setToggleIcon('navToggle')
+    }
+
     return (
         <nav className='navbar'>
             <div className="logo">
                 <h2>Igor Trindade.dev</h2>
             </div>
 
-            <ul className="navLinks">
+            <ul className={active}>
                 <li className="navLink"><a href="#">Home</a></li>
                 <li className="navLink"><a href="#">About</a></li>
                 <li className="navLink"><a href="#">Services</a></li>
                 <li className="navLink"><a href="#">Projects</a></li>
                 <li className="navLink">
-                    <button className="navButton">Contact</button>
+                    <button className="navButton">Contact
+                    </button>
                 </li>
             </ul>
+
+            <div className={toggleIcon} onClick={navToggle}>
+                <div className="line1"></div>
+                <div className="line2"></div>
+                <div className="line3"></div>
+            </div>
         </nav>
     );
 }
