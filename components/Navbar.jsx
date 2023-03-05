397fca123fdbe05dfaee7c530418d81efad58958
Igor Trindade <84097839+igortrindadelg@users.noreply.github.com>
2023-03-04 22:03:33 -0300

    hovers adicionados

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,9 @@
 import styles from "../styles/Navbar.module.scss";
-import altStyles from "../styles/NavbarMobileActive.module.scss";
 import Link from "next/link";
 import Image from "next/image";
 import logo from "../public/assets/logo.png";
 import logoBranco from "../public/assets/logoBranco.png";
-import { BiMenu } from "react-icons/bi";
-import { IoCloseCircle } from "react-icons/io";
+import { BiMenu, BiX } from "react-icons/bi";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -16,31 +14,45 @@ export default function Navbar() {
   }
 
   return (
-    <nav className={menuActive ? altStyles.navbar : styles.navbar}>
-      <Image className={altStyles.logo} src={logo} alt="Logo - Igor Trindade" />
+    <nav className={styles.navbar}>
+      <Image className={styles.logo} src={logo} alt="Logo - Igor Trindade" />
 
-      <ul className={menuActive ? altStyles.menuLista : styles.menuLista}>
-        <li className={menuActive ? altStyles.menuLinks : styles.menuLista}>
+      <ul
+        className={`${styles.menuLista} ${
+          menuActive ? styles["menuListaActive"] : styles["menuListaExit"]
+        }`}
+      >
+        <li className={styles.menuLinks}>
           <Link href="#">Home</Link>
         </li>
-        <li className={menuActive ? altStyles.menuLinks : styles.menuLinks}>
+        <li className={styles.menuLinks}>
           <Link href="#sobreMim">Sobre Mim</Link>
         </li>
-        <li className={menuActive ? altStyles.menuLinks : styles.menuLinks}>
+        <li className={styles.menuLinks}>
           <Link href="#">Projetos</Link>
         </li>
-        <li className={menuActive ? altStyles.menuLinks : styles.menuLinks}>
+        <li className={styles.menuLinks}>
           <Link href="#">Contato</Link>
         </li>
       </ul>
 
-      <button
-        aria-label="Abrir menu"
-        className={menuActive ? altStyles.iconMenu : styles.iconMenu}
-        onClick={handleMenuActive}
-      >
-        <BiMenu />
-      </button>
+      {menuActive ? (
+        <button
+          aria-label="Fechar menu"
+          className={styles.iconFechar}
+          onClick={handleMenuActive}
+        >
+          <BiX />
+        </button>
+      ) : (
+        <button
+          aria-label="Abrir menu"
+          className={styles.iconMenu}
+          onClick={handleMenuActive}
+        >
+          <BiMenu />
+        </button>
+      )}
     </nav>
   );
 }
